118f82626f505ed2149e28799093f8373686cde8
Rebeca  Freitas <rebecasguerri@gmail.com>
2023-08-24 15:16:19 -0300

    update

---
+++ js.js
@@ -0,0 +1,8 @@
+const  inputDay = document.querySelector(".input-day")
+const  inputMonth = document.querySelector(".inpu-month")
+const  inputYear = document.querySelector(".")
+const 
+const 
+const 
+const 
+const 
