2a0c84f1fbd6f932788124118f2e850f5b20b4c8
Rebeca  Freitas <rebecasguerri@gmail.com>
2023-08-25 16:00:36 -0300

    update

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 
-const  inputDay = document.querySelector(".input-day");
-const  inputMonth = document.querySelector(".input-month");
-const  inputYear = document.querySelector(".input-year");
+const  inputDay = document.querySelector(".input-day")
+const  inputMonth = document.querySelector(".input-month")
+const  inputYear = document.querySelector(".input-year")
 const  submitBtn = document.querySelector(".btn")
 const  outputDay = document.querySelector(".output-day");
 const  outputMonth = document.querySelector(".output-month");
@@ -9,39 +9,54 @@ const  outputYear = document.querySelector(".output-year");
 const  errorDay = document.querySelector(".error-day");
 const  errorMonth = document.querySelector(".error-month");
 const  errorYear = document.querySelector(".error-year");
-const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-const form = document.querySelector("form");
 const date = new Date();
-const day = date.getDate();
-const month = 1 + date.getMonth();
-const year = date.getFullYear();
+const d2 = date.getDate();
+const m2 = 1 + date.getMonth();
+const y2 = date.getFullYear();
+const month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-console.log(errorDay, errorMonth, errorYear)
 
-console.log(inputDay, inputMonth, inputYear)
 
-inputDay.addEventListener("input", function validateday() {
+inputDay.addEventListener("input", function(){
     if(inputDay.value > 31  ){
         errorDay.style.display = "block"
     }else{
         errorDay.style.display = "none"
     }
 })
-inputMonth.addEventListener("input", function validatemonth() {
+inputMonth.addEventListener("input", function(){
     if(inputMonth.value > 12 ){
         errorMonth.style.display = "block"
     }else{
         errorMonth.style.display = "none"
     }
 })
-inputYear.addEventListener("input", function validateyear() {
-    if(inputYear.value > year ){
+inputYear.addEventListener("input", function (){  
+    if(inputYear > y2 ){
         errorYear.style.display = "block"
     }else{
         errorYear.style.display = "none"
     }
 })
+ function validate(){ 
+    if(inputYear.value > y2){
+        y2 = y2 - inputYear.value
+        y2 = y2 + month[y2 - 1];
+        m2 = m2 - 1;
+    }
+    if(inputMonth.value> m2){
+        m2 = m2 + 12;
+        y2 = y2 -1;
+    }
+
+    const d = d2 - inputDay.value;
+    const m = m2 - inputMonth.value;
+    const y = y2 - inputYear.value;
+
+    outputDay.innerHTML = d , "roda";
+    outputMonth.innerHTML = m, "roda";
+    outputYear.innerHTML = y, "roda"
+ }
 
 
-
-
+ submitBtn.addEventListener("onclick", validate())
